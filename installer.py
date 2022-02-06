4ac9a2efe30f082de18ca9cdefe2c82894e834f0
blurrrb <17aakashsingh1999@gmail.com>
2022-02-06 18:39:13 +0530

    Refactor

--- installer.py
+++ installer.py
@@ -5,6 +5,8 @@ DEFAULT_USERNAME = 'blrb'
 DEFAULT_HOSTNAME = 'rogstrix'
 DEFAULT_DRIVE = '/dev/sda'
 
+DEFAULT_GITHUB_USERNAME = 'blurrrb'
+
 # PACKAGES
 PKGS_BOOTLOADER = [
     'grub',
@@ -101,89 +103,77 @@ def get_input_or_default(message, default):
     return inp if len(inp) > 0 else default
 
 
-def arch_chroot(user):
-    commands = []
+def exec_arch_chroot(user, commands):
+    payload = '\n'.join(commands)
+    shell(f'arch-chroot -u {user} /mnt bash -c \'{payload}\'')
 
-    def chroot_shell(cmd):
-        commands.append(cmd)
 
-    def execute():
-        payload = '\n'.join(commands)
-        shell(f'arch-chroot -u {user} /mnt bash -c \'{payload}\'')
-
-    return chroot_shell, execute
+def exec(commands):
+    payload = '\n'.join(commands)
+    shell(f'bash -c \'{payload}\'')
 
 
 def main():
     user = get_input_or_default(
-        f'Enter username: [{DEFAULT_USERNAME}]', DEFAULT_USERNAME)
+        f'Enter username: [{DEFAULT_USERNAME}] ', DEFAULT_USERNAME)
     hostname = get_input_or_default(
-        f'Enter hostname: [{DEFAULT_HOSTNAME}]', DEFAULT_HOSTNAME)
+        f'Enter hostname: [{DEFAULT_HOSTNAME}] ', DEFAULT_HOSTNAME)
+
+    github_username = get_input_or_default(
+        f'Enter github username:[{DEFAULT_GITHUB_USERNAME}] ', DEFAULT_GITHUB_USERNAME)
+    
+    github_email = input('Enter github email: ')
 
     shell('fdisk -l')
     drive = get_input_or_default(
         f'Enter drive: [{DEFAULT_DRIVE}]', DEFAULT_DRIVE)
 
-    shell('systemctl stop reflector')
-    shell('reflector --save /etc/pacman.d/mirrorlist --country India --protocol https --sort rate && cat /etc/pacman.d/mirrorlist')
+    exec([
+        f'systemctl stop reflector',
+        f'reflector --save /etc/pacman.d/mirrorlist --country India --protocol https --sort rate',
+        f'cat /etc/pacman.d/mirrorlist',
+        f'timedatectl set-ntp true',
+        f'parted {drive} mklabel gpt',
+        f'parted {drive} mkpart "EFI-System-Partition" fat32 1Mib 512Mib set 1 esp on',
+        f'parted {drive} mkpart "Linux-Partition" ext4 512MiB 100%',
+        f'mount {drive}2 /mnt',
+        f'mkdir /mnt/boot',
+        f'mount {drive}1 /mnt/boot',
+        f'pacstrap -i /mnt base linux-lts linux-lts-headers linux-firmware',
+        f'genfstab -U /mnt >> /mnt/etc/fstab'
+    ])
 
-    shell('timedatectl set-ntp true')
+    exec_arch_chroot('root', [
+        f'pacman -S {PKGS}',
+        f'ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime',
+        f'hwclock --systohc',
+        f'echo \'en_US.UTF-8 UTF-8\' > /etc/locale.gen',
+        f'locale-gen',
+        f'echo \'LANG=en_US.UTF-8\' > /etc/locale.conf',
+        f'echo {hostname} >> /etc/hostname',
+        f'mkinitcpio -P',
+        f'grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg',
+        f'sed -i \'s/^#\s*\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/\' /etc/sudoers',
+        f'usermod -m -G wheel,libvirt,docker {user}',
+        f'echo \'Enter root password:\' && passwd',
+        f'echo \'Enter {user} password:\' && passwd {user}',
+        f'systemctl enable NetworkManager',
+        f'systemctl enable sddm',
+        f'systemctl enable libvirtd',
+        f'systemctl enable docker',
+        f'reflector --save /etc/pacman.d/mirrorlist --country India --protocol https --sort rate'
+    ])
 
-    shell(
-        f'''parted {drive} << EOF
-        mklabel gpt
-        mkpart "EFI System Partition" fat32 1Mib 512Mib set 1 esp on
-        mkpart "Linux Partition" ext4 512MiB 100 %
-        EOF
-        mount {drive}2 /mnt
-        mkdir /mnt/boot
-        mount {drive}1 /mnt/boot''')
-
-    input('debug checkpoint')
-
-    shell('pacstrap -i /mnt base linux-lts linux-lts-headers linux-firmware')
-    shell('genfstab -U /mnt >> /mnt/etc/fstab')
-
-    # chroot as root
-    root_chroot_shell, root_chroot_execute = arch_chroot('root')
-
-    root_chroot_shell(f'pacman -S {PKGS}')
-    root_chroot_shell('ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime')
-    root_chroot_shell('hwclock --systohc')
-    root_chroot_shell('echo \'en_US.UTF-8 UTF-8\' > /etc/locale.gen')
-    root_chroot_shell('locale-gen')
-    root_chroot_shell('echo \'LANG=en_US.UTF-8\' > /etc/locale.conf')
-    root_chroot_shell(f'echo {hostname} >> /etc/hostname')
-    root_chroot_shell('mkinitcpio -P')
-
-    root_chroot_shell(
-        'grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg')
-
-    root_chroot_shell(
-        'sed -i \'s/^#\s*\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/\' /etc/sudoers')
-    root_chroot_shell(f'usermod -m -G wheel,libvirt,docker {user}')
-
-    root_chroot_shell('echo \'Enter root password:\' && passwd')
-    root_chroot_shell(f'echo \'Enter {user} password:\' && passwd {user}')
-    root_chroot_shell('systemctl enable NetworkManager')
-    root_chroot_shell('systemctl enable sddm')
-    root_chroot_shell('systemctl enable libvirtd')
-    root_chroot_shell('systemctl enable docker')
-
-    root_chroot_shell('reflector --save /etc/pacman.d/mirrorlist --country India --protocol https --sort rate')
-
-    root_chroot_execute()
-
-    user_chroot_shell, user_chroot_execute = arch_chroot(user)
-
-    user_chroot_shell('git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin')
-    user_chroot_shell('cd /tmp/yay-bin && makepkg -si')
-    user_chroot_shell(f'yay -S {AURS}')
-
-    user_chroot_execute()
+    exec_arch_chroot(user, [
+        f'git config --global user.name {github_username}',
+        f'git config --global user.email {github_email}',
+        f'git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin'
+        f'cd /tmp/yay-bin && makepkg -si'
+        f'yay -S {AURS}'
+    ])
 
     shell('umount /mnt/boot && umount /mnt')
 
-    
+
 if __name__ == '__main__':
     main()
